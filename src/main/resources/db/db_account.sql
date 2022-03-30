5b2f1a8c36556dcd54fc35604d2916f3735e8a5d
hani <violetear24@naver.com>
2022-03-30 12:48:56 +0900

    가데이터 추가

--- src/main/resources/db/db_account.sql
+++ src/main/resources/db/db_account.sql
@@ -12,10 +12,10 @@ a_answer varchar2(30 char) not null
 
 select * from account_mio;
 delete from account_mio;
-insert into ACCOUNT_MIO values ('hn','hn','hn','경기 성남시 중원구 사기막골로62번길 18!11!13210','01000000000','img','admin','q4','a');
-insert into ACCOUNT_MIO values ('dj','dj','dj','경기 광진구 자양강변길 7!111!05088','01000000000','dj.jpg','admin','q1','a');
-insert into ACCOUNT_MIO values ('yj','yj','yj','경기 성남시 중원구 사기막골로62번길 18!11!13210','01000000000','yj.jpg','admin','q2','a');
-insert into ACCOUNT_MIO values ('jh','jh','jh','경기 안성시 일죽면 판교길 6!11!17532','01000000000','jh.jpg','admin','q3','a');
+insert into ACCOUNT_MIO values ('hntest','hn','hn','경기 성남시 중원구 사기막골로62번길 18!11!13210','01000000000','hn.jpg','admin','q4','a');
+insert into ACCOUNT_MIO values ('djtest','dj','dj','경기 광진구 자양강변길 7!111!05088','01000000000','dj.jpg','general','q1','a');
+insert into ACCOUNT_MIO values ('yjtest','yj','yj','경기 성남시 중원구 사기막골로62번길 18!11!13210','01000000000','yj.jpg','seller','q2','a');
+insert into ACCOUNT_MIO values ('jhtest','jh','jh','경기 안성시 일죽면 판교길 6!11!17532','01000000000','jh.jpg','general','q3','a');
 
 select * from account_sellerjoin;
 
@@ -32,3 +32,5 @@ s_answer varchar2(30 char) not null,
 s_intro varchar2(500 char) not null,
 s_sellsort varchar2(20 char) not null
 );
+
+insert into account_sellerjoin values('seller1','111111','seller','경기 성남시 중원구 사기막골로62번길 18!11!13210','01000000000','seller1.png','seller','q1','a','sellerintro','fashion');
